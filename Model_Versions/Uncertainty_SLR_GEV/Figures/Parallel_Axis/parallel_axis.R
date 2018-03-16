992b2589e46d67a5addeaf25df102b50b3ee7c58
Perry Oddo <pcoddo@gmail.com>
2018-03-16 15:21:39 -0400

    Clean up plotting function

--- Model_Versions/Uncertainty_SLR_GEV/Figures/Parallel_Axis/parallel_axis.R
+++ Model_Versions/Uncertainty_SLR_GEV/Figures/Parallel_Axis/parallel_axis.R
@@ -1,125 +1,130 @@
-#load package for parallel coordinate plot
-library(MASS)
-library(fields)
-library(sfsmisc)
+###################################
+# file: parallel_axis.R
+###################################
+# Author and copyright: Perry Oddo
+# Pennsylvania State University
+# poddo@psu.edu
+###################################
+# Produces parallel axis plot to 
+# illustrate tension between objectives
+#################################### 
 
-load(file = "../../../../../vanDantzig_largefiles/Model_Versions/Uncertainty_SLR_GEV/SLR_GEV.RData")
+# Plot function
+plot.parallel <- function(obj, comp, labels=colnames(obj), col.line=rep(par("fg"), nrow(obj)), col.text=par("fg"), make.plot=TRUE, ...) {
+  obj.mat.new <- obj
+  n.objs <- ncol(obj)-1
+  
+  obj.range <- apply(obj[,1:n.objs], 2, range)
+  obj <- normalize.objective(obj[,1:n.objs])
+  
+  comp <- normalize.objective(comp)
+  
+  print(obj.range)
+  if(make.plot){
+    plot.new()
+    par(xpd=T)
+    par(mar=c(6,3.5,4,2.5)+0.1)
+    plot.window(xlim=c(1,n.objs), ylim=c(0,1), yaxs="i")
+    axis(1, at=1:n.objs, labels=F, line=2, lwd = 2)
+    mtext(1, at=1:n.objs, text=labels, col=col.text[1:n.objs], line=2, font=1, padj = 1)
+    arrows(x0=0.9, y0=0.95, y1=0.05, length=0.1, lwd = 2)
+    mtext("Preference", 2, font = 2)
+    par(xpd=F)
+    invisible(sapply(1:nrow(comp), function(this.item) lines(comp[this.item,], col="light gray", lty = 1, lwd = 0.75)))
+    #invisible(sapply(1:nrow(comp), function(this.item) lines(comp[this.item,], col=rgb(128/255, 128/255, 128/255, alpha = 0.25), lty = 1, lwd = 0.5)))
+    abline(v=1:n.objs, lty=1, lwd=2)
+    par(xpd=T)
+    lines(1:n.objs, rep(0, n.objs), col=par("fg"), lty=3, lwd=3)
+    for(i in 1:nrow(obj.mat.new)){
+      if(obj.mat.new[i,5]==1){
+        lines(obj[i,], col = col.line[i], ...)
+        points(obj[i,], bg = col.line[i], col = "black", pch = 22, cex = 1.25, lwd = 2)
+        #invisible(sapply(1:nrow(obj), function(this.item) lines(obj[this.item,], col=col.line[this.item], ...)))
+      } else {
+        lines(obj[i,], col = col.line[i], lty = 2, ...)
+        points(obj[i,], bg = col.line[i], col = "black", pch = 21, cex = 1.25, lwd = 2)
+        #invisible(sapply(1:nrow(obj), function(this.item) lines(obj[this.item,], col=c("red", "black"), lty = 2)))
+      }
+    }
+    #invisible(sapply(1:nrow(obj), function(this.item) lines(obj[this.item,], col=col.line[this.item], ...)))
+    #invisible(sapply(1:nrow(obj), function(this.item) points(obj[this.item,], col = rep("black", nrow(obj)), bg = col.line[this.item],pch = 22, lwd = 3, cex = 1.25)))
+    par(xpd=F)
+    axis(1, at = 1L:ncol(obj), labels = pretty10exp(signif(obj.range[1,]), digits = 3), lty = 0, line = -0.75, cex.axis = 1, col.axis = "black")
+    axis(3, at = 1L:ncol(obj), labels = pretty10exp(signif(obj.range[2,]), digits = 3), lty = 0, line = -0.75, cex.axis = 1, col.axis = "black")
+    
+    #axis(1, at = 1L:ncol(obj), labels = pretty10exp(min.vals), lty = 0, line = -0.85, cex.axis = 1, col.axis = "black")
+    #axis(3, at = 1L:ncol(obj), labels = pretty10exp(max.vals), lty = 0, line = -0.85, cex.axis = 1, col.axis = "black")
+  
+  }
+  
+  invisible(list(obj.norm = obj))
+  
+}
 
-#read data 
+### Find min/max boundaries for each objective
 M = Objectives
 min.vals <- sapply(1:ncol(M), function(x) 
-  {
+{
   signif(min(M[,x]), digits = 3)
-  })
+})
 
 max.vals <- sapply(1:ncol(M), function(x) 
-  {
+{
   signif(max(M[,x]), digits = 3)
-  })
+})
 
-min.vals <- signif(min.vals, digits = 3)
-# define "brushing" thresholds
-# useful to focus on a few solutions instead of all at once
-# thresholds row format: column number, threshold value, less than (-1) or greater than (1)
-# not all columns need to have thresholds applied
+unit.vals <- c("$", "$", "$", "1/yr")
 
-thresholds <- matrix(c(4, (1/100),-1), nrow = 1, ncol = 3, byrow = TRUE)
+### Find optimal solutions for each objective
+# Total costs:
+total_costs.index = which.min(Objectives$total_costs.v)
 
-#determine solutions to be brushed
-IX <- seq(1,dim(M)[1])
-
-
-for (i in 1:nrow(thresholds))
-{
-  col <- thresholds[i ,1]
-  if(thresholds[i,3]==-1){
-    tempIX <- rfind <- seq(along=M[ ,col])[as.logical(M[ ,col] < thresholds[i,2])]
-  }
-  else{
-    tempIX <- rfind<- seq(along=M[ ,col])[as.logical(M[ ,col] > thresholds[i,2])]
-  }
+# Costs:
+  # Find all indices where costs are zero
+  zero.cost = which(Objectives$costs.v==0)
   
-  IX <- intersect(IX,tempIX)
+  # Sum Objectives across all states of the world
+  obj.sum = apply(Objectives, 1, sum)
+
+  # Find SOW where costs == 0 AND other objectives are minimized
+  cost.index = zero.cost[which.min(obj.sum[zero.cost])]
+
+# Damages  
+damages.index = which.min(Objectives$NPV_expected_losses.v)
+
+# Flood Probability
+flood.index = which.min(Objectives$EV_p_exceed_transient.v)
+
+### Find compromise solutions
+  # Solution with highest flood probability
+  #comp1 <- which.max(Objectives$EV_p_exceed_transient.v)
   
-}
-
-brush_on_idx <- unique(IX)
-brush_off_idx <- setdiff(seq(1,nrow(M)),brush_on_idx)
-
-
-#re-map the values in M to [0, 1] for consistent plotting
-ncols = ncol(M)
-for (i in 1:ncols){
-  M[ ,i] <- (M[ ,i] - min(M[ ,i], na.rm=T))/(max(M[ ,i], na.rm=T) - min(M[ ,i], na.rm=T))
-}
-
-# brush_on_idx now contains the indices of the rows of M that satisfy:
-# brush_off_idx contains the remaining indices that do not satisfy these.
-
-
-#make color_map.  This one scales based on column 1 values
-col_to_plot <- M[brush_on_idx,4]
-
-ColorRamp <- c(colorRampPalette(c("blue", "light blue"))(50),
-               colorRampPalette(c("light blue", "light green", "yellow"))(55),
-               colorRampPalette(c("yellow", "orange", "red", "darkred"))(70))
-
-ColorRampAlpha <- apply(sapply(ColorRamp, col2rgb)/255, 2, 
-                        function(x) rgb(x[1], x[2], x[3], alpha=0.1))
-
-ColorLevels <- seq(min(col_to_plot), 1, length=length(ColorRamp))
-
-z_scl <- (col_to_plot - min(col_to_plot, na.rm=T))/
-  (max(col_to_plot, na.rm=T) - min(col_to_plot, na.rm=T))
-color_scl = round(z_scl*length(ColorRamp))
-color_scl[color_scl == 0] = 1
-
-#concatenate brushed off and brushed on indices in a matrix for plotting
-all_solutions =rbind(M[brush_off_idx, ],M[brush_on_idx, ]) 
-
-colnames(all_solutions) <- c("Total Costs", "Costs", "Expected Damages", "Flood Probability")
-
-# Set layout to include a colorbar next to plot.
-# Parallel coord function:
-parcoord2 <- function (x, col = 1, lty = 1, var.label = FALSE, ...) 
-{
-  rx <- apply(x, 2L, range, na.rm = TRUE)
-  x <- apply(x, 2L, function(x) (x - min(x, na.rm = TRUE))/(max(x, 
-                                                                na.rm = TRUE) - min(x, na.rm = TRUE)))
+  # Solution which maximizes sum of all objectives (performs poorly)
+  comp2 <- which.min(abs(Objectives$costs.v - quantile(Objectives$costs.v, 0.99)))
+  #comp2 <- which.max(obj.sum)
   
-  matplot(1L:ncol(x), t(x), type = "l", col = col, lty = lty, lwd = 0.65, 
-          xlab = "", ylab = "", axes = FALSE, ...)
-  axis(1, at = 1L:ncol(x), labels = colnames(x), lty = 0)
-  axis(1, at = 1L:ncol(x), labels = pretty10exp(min.vals), lty = 0, line = -1, cex.axis = 0.65, col.axis = "gray48")
-  axis(3, at = 1L:ncol(x), labels = pretty10exp(max.vals), lty = 0, line = -1, cex.axis = 0.65, col.axis = "gray48")
+  # Solution which shows 99% quantile of total costs
+  #comp3 <- which.min(abs(Objectives$total_costs.v - quantile(Objectives$total_costs.v, 0.99)))
   
-  abline(v=1:ncol(x), lty=1, lwd=1.5)
-  for (i in 1L:ncol(x)) {
-    lines(c(i, i), c(0, 1), col = "black")
-    if (var.label) 
-      text(c(i, i), c(0, 1), labels = format(rx[, i], digits = 3), 
-           xpd = NA, offset = 0.3, pos = c(1, 3), cex = 0.7)
-  }
-  invisible()
-}
+  comp4 = Objectives[1669726,]
+  comp5 = Objectives[1751766,]
+### Combine optimal and compromise solutions into matrix for plotting
+# In 5th column, 1 denotes optimal while -1 denotes compromise
 
-#plot solutions
-png("parallel_axis.brush.png", width = 6.5, height = 4.5, unit = 'in', res  = 300)
-par(mar = c(8,2,1,2), oma = c(0,0.5,1,0.5), font = 1)
-parcoord2(all_solutions, 
-         col=c(rep.int("grey",length(brush_off_idx)),ColorRampAlpha[color_scl]), 
-         var.label=FALSE,
-         xaxt = "n")
-arrows(x0=0.91, y0=0.95, y1=0.05, length=0.1, lwd = 2)
-mtext("Preference", side = 2, line = 1, cex = par("cex.lab"))
+#obj.cols = c("dark blue", "skyblue3", "green", "darkorchid4", "indianred", "orange")
+obj.cols = c("darkorchid", "indianred", "green", "dodgerblue", "sky blue", "orange")
+#obj.cols = c(rainbow(6)[5], rainbow(6)[6], rainbow(6)[1], rainbow(6)[2], rainbow(6)[4], rainbow(6)[3])
+obj.mat = matrix(data = c(as.numeric(Objectives[total_costs.index,]), 1,
+            as.numeric(Objectives[cost.index,]), 1,
+            as.numeric(Objectives[damages.index,]), 1,
+            as.numeric(Objectives[flood.index,]), 1,
+            as.numeric(comp5), -1, 
+            as.numeric(comp4), -1 ),nrow = 6, ncol = 5, byrow = T)
+comp.mat = as.matrix(Objectives[sample(1:nrow(Objectives), 200, replace = F),], nrow = 200, ncol = 4, byrow = T)
 
-# Add colorbar
-image.plot(1, ColorLevels, add = T, legend.line = 6, smallplot =  c(0.15,0.85,0.06,0.1),
-      matrix(data=ColorLevels, ncol=length(ColorRamp),nrow=1), legend.only = T, 
-      col=ColorRamp,xlab="",ylab="Column 1 Value",xaxt="n", las = 1, axes = F, horizontal = T,
-      legend.shrink = 0.75, legend.width = 0.85, 
-      axis.args = list(at = c(0,1), labels = c(pretty10exp(min.vals)[4], pretty10exp(1e-02)), tick = F, line = -0.85, cex.axis = 0.85),
-      legend.args = list(side = 3, line = 0.35, text = "Flood Probability for brushed set (1/yr)", cex = 0.85))
-
-dev.off()
-### End of Plotting
+### Plot
+pdf("parallel_axis.pdf", width = 6.5, height = 4.25)
+plot.parallel(obj.mat, comp = comp.mat, labels = c("Total Costs", "Investment \nCosts", "Expected \nDamages", "Flood \nProbability"),
+              col.line = obj.cols,
+              col.text = obj.cols, lwd = 2, type = 'l', pch = 22, cex = 1.25, bg = obj.cols)
+dev.off() 
